e04339cd9713ea9729849ac5939eb885fe12bd06
Hidayt Rahman <hidaytrahman@gmail.com>
2023-05-28 17:39:41 +0530

    added data for browsers

---
+++ src/data/index.ts
@@ -0,0 +1,54 @@
+export const browsers = [
+	{
+		id: '1',
+		title: 'chrome',
+		icon: 'chrome',
+		type: 'browser',
+		link: '',
+		extensions: [
+			{
+				id: 'chromee1',
+				title: 'React Developer Tools',
+				text: '',
+				link: '',
+			},
+			{
+				id: 'chromee2',
+				title: 'Redux',
+				text: '',
+				link: '',
+			},
+			{
+				id: 'chromee3',
+				title: 'JSON Beautifier',
+				text: '',
+				link: '',
+			},
+		],
+	},
+	{
+		id: '1',
+		title: 'firefox',
+		icon: 'firefox',
+		type: 'browser',
+		link: '',
+		extensions: [],
+	},
+	{
+		id: '1',
+		title: 'safari',
+		icon: 'safari',
+		type: 'browser',
+		link: '',
+		extensions: [],
+	},
+
+	{
+		id: '1',
+		title: 'edge',
+		icon: 'edge',
+		type: 'browser',
+		link: '',
+		extensions: [],
+	},
+];
